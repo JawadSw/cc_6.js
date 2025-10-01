e42930220fa7875eaeda41b584a08158da060a9a
Jawad Sweilem <jsweilem@usf.edu>
2025-10-01 14:19:32 -0400

    Made a class called Employee. added a function to describe the employee.

---
+++ cc_6.js
@@ -0,0 +1,14 @@
+class Employee {
+    constructor(name, department) {
+        this.name= name;
+        this.department = department;
+    }
+
+    describe(){
+        return `${this.name} is from the ${this.department} department.`
+    }
+}
+
+employees = [
+    
+]
