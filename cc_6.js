e3a1dc3c49a04d2502225c30161630649ec4a718
Jawad Sweilem <jsweilem@usf.edu>
2025-10-05 21:04:56 -0400

    did step 3

--- cc_6.js
+++ cc_6.js
@@ -1,14 +1,37 @@
 class Employee {
-    constructor(name, department) {
-        this.name= name;
-        this.department = department;
-    }
+  constructor(name, department, level = "Associate") {
+    this.name = name;
+    this.department = department;
+    this.level = level;
+  }
 
-    describe(){
-        return `${this.name} is from the ${this.department} department.`
+  describe() {
+    return `${this.name} is a ${this.level} in ${this.department}`;
+  }
+
+  promote() {
+    if (this.level === "Associate") {
+      this.level = "Manager";
+    } else if (this.level === "Manager") {
+      this.level = "Director";
+    } else {
+      this.level = "Executive";
     }
+  }
 }
 
-employees = [
-    
-]
+//step 3
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department, "Manager");
+    this.teamSize = teamSize;
+  }
+
+  
+  describe() {
+    return `${this.name} manages ${this.teamSize} people in ${this.department}`;
+  }
+}
+
+
+
